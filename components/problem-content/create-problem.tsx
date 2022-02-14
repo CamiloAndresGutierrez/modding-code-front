e9f65c312fcfbe768f0a52f4034f7347928b3336
Camilo Andres Gutierrez Torres <camilo1999gutierrez@gmail.com>
2022-02-14 03:01:11 -0500

    added problem creation

--- components/problem-content/create-problem.tsx
+++ components/problem-content/create-problem.tsx
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CreateProblem = () => {
+  const [ problemName, setProblemName ] = useState("");
+  const [ difficulty, setDifficulty ] = useState("1");
+  const [ description, setDescription ] = useState("");
+
+  const handleInputChange = (e, type) => {
+    const value = e.target.value;
+    switch (type) {
+      case "name":
+        setProblemName(value);
+        break;
+      case "description":
+        setDescription(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleDifficultyChange = (e) => {
+    const value = e.target.value;
+    setDifficulty(value);
+  };
+
+  const handleSubmit = () => {
+    const isFilled = [ problemName, description].some(element => element.length === 0);
+    if (!isFilled) {
+      console.log(problemName);
+      console.log(difficulty);
+      console.log(description);
+      setProblemName("");
+      setDescription("");
+      setDifficulty("1");
+    }
+  }
+
   return(
-    <>Hello</>
+    <form>
+      <input value={problemName} placeholder={"Name"} onChange={(e) => handleInputChange(e, "name")}></input>
+      <select value={difficulty} onChange={(e) => handleDifficultyChange(e)}>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </select>
+      <textarea value={description} onChange={(e) => handleInputChange(e, "description")}></textarea>
+      <button type={"button"} onClick={() => handleSubmit()}>Create</button>
+    </form>
   )
 };
 
--- components/problems-creation/index.tsx
+++ components/problems-creation/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProblemContent from 'components/problem-content';
 import CreateProblem from 'components/problem-content/create-problem';
 
 
 const ProblemsCreation = ({ problems }) => {
+  const [ isNewProblem, setIsNewProblem ] = useState(false);
+  const handleCreateProblem = () => {
+    setIsNewProblem(!isNewProblem);
+  }
+
   return (
     <div>
-      <CreateProblem />
+      <button onClick={() => handleCreateProblem()}>{isNewProblem ? "Cancel" : "Create problem"} </button>
+      {
+        isNewProblem && (
+          <CreateProblem />
+        )
+      }
       {
         problems.map(problem => (
           <ProblemContent
