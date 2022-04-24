28633de34905b4d88a22f176c84433043f061960
Camilo Andres Gutierrez Torres <camilo1999gutierrez@gmail.com>
2022-04-23 22:13:45 -0500

    Updated mobile design for navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Name from '../Name';
 import { Container, NavButton } from './navbar.styled-components';
 import { studentLinks, expertLinks } from './navbar.content';
-import { UserType } from 'lib/types';
 import Link from 'next/link';
-import { IdToken, useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from '@auth0/auth0-react';
 import router from 'next/router';
-import decode from 'jwt-decode';
+import MenuIcon from '@mui/icons-material/Menu';
 
 type NavbarProps = {
     height?: string;
@@ -16,6 +15,7 @@ type NavbarProps = {
 const Navbar = (props: NavbarProps) => {
     const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
     const [links, setLinks] = useState([]);
+    const [shouldDisplayMobileNav, setShouldDisplayMobileNav] = useState(false);
     const { height, userType } = props;
 
     useEffect(() => {
@@ -32,8 +32,45 @@ const Navbar = (props: NavbarProps) => {
         }
     }, [userType]);
 
+    const renderLinks = () => {
+        return (
+            <>
+                {
+                    (links.length > 0) &&
+                    links.map(element => (
+                        <li key={element.label}>
+                            <Link href={element.path} passHref={true}>
+                                {element.label}
+                            </Link>
+                        </li>
+                    ))
+                }
+                {
+                    isAuthenticated ?
+                        <li >
+                            <NavButton onClick={() => logout()}>Log out</NavButton>
+                        </li> :
+                        <>
+                            <li >
+                                <NavButton onClick={() => loginWithRedirect()}>Log in</NavButton>
+                            </li>
+                            <li >
+                                <NavButton onClick={() => router.push("/signup")}>Signup</NavButton>
+                            </li>
+                        </>
+                }
+            </>
+        )
+    }
+    const handleMobileNav = () => {
+        setShouldDisplayMobileNav(!shouldDisplayMobileNav)
+    }
+
     return (
-        <Container height={height}>
+        <Container
+            height={height}
+            displayMobileNav={shouldDisplayMobileNav}
+        >
             <nav>
                 <div className="logo">
                     <Link href='/' passHref>
@@ -42,34 +79,17 @@ const Navbar = (props: NavbarProps) => {
                         </div>
                     </Link>
                 </div>
-
                 <ul className="links">
-                    {
-                        (links.length > 0) &&
-                        links.map(element => (
-                            <li key={element.label}>
-                                <Link href={element.path} passHref={true}>
-                                    {element.label}
-                                </Link>
-                            </li>
-                        ))
-                    }
-                    {
-                        isAuthenticated ?
-                            <li >
-                                <NavButton onClick={() => logout()}>Log out</NavButton>
-                            </li> :
-                            <>
-                                <li >
-                                    <NavButton onClick={() => loginWithRedirect()}>Log in</NavButton>
-                                </li>
-                                <li >
-                                    <NavButton onClick={() => router.push("/signup")}>Signup</NavButton>
-                                </li>
-                            </>
-                    }
+                    {renderLinks()}
                 </ul>
+
+                <div className="burger-icon" onClick={() => handleMobileNav()}>
+                    <MenuIcon />
+                </div>
             </nav>
+            <div className="links-mobile-bar">
+                {renderLinks()}
+            </div>
         </Container >
     )
 }
--- components/navbar/navbar.styled-components.tsx
+++ components/navbar/navbar.styled-components.tsx
@@ -1,6 +1,7 @@
+import { mediaQueries } from 'lib/constants';
 import styled from 'styled-components';
 
-export const Container = styled.div<{ height }>`
+export const Container = styled.div<{ height: string, displayMobileNav: boolean }>`
     height: ${props => props.height ?? "100px"};
     background-color: white;
     box-shadow: 0px 10px 50px 10px rgba(0, 0, 0, 0.1);
@@ -14,15 +15,19 @@ export const Container = styled.div<{ height }>`
         display: flex;
         justify-content: space-between;
         align-items: center;
-        position: absolute;
+
+        position: relative;
         top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
+        z-index: 2;
+        
         margin: auto;
+        @media (${mediaQueries.mobile}) {
+            padding: 0 10px;
+        }
     }
 
     .logo {
+        height: ${props => props.height ?? "100px"};
         display: flex;
         align-items: center;
     }
@@ -34,6 +39,55 @@ export const Container = styled.div<{ height }>`
         align-items: center;
         list-style-type: none;
         gap: 20px;
+
+        @media (max-width: 513px) {
+            display: none;
+        }
+    }
+
+    .burger-icon {
+        @media (min-width: 513px) {
+            display: none;
+        }
+
+        @media (max-width: 513px) {
+            display: block;
+        }
+    }
+
+    .links-mobile-bar {
+        height: 100px;
+        width: 100%;
+        background-color: white;
+        
+        position: relative;
+        top: -100px;
+        z-index: 1;
+
+        box-shadow: 0px 20px 10px -15px rgba(0, 0, 0, 0.1);
+        transition: all 500ms ease-in-out;
+
+        ${props => props.displayMobileNav ?
+            `
+                opacity: 1;
+                top: 0;
+            ` :
+            `
+                opacity: 0;
+            `
+        }
+
+        @media (min-width: 513px) {
+            display: none;
+        }
+
+        @media (max-width: 513px) {
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+            list-style-type: none;
+            gap: 20px;
+        }
     }
 `;
 
